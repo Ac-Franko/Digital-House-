6e189cb23573c6b3c2ea95cacd815a2dafefb1f5
Ana Cecília Franco <86064186+Ac-Franko@users.noreply.github.com>
2021-12-06 08:16:52 -0300

    Add files via upload

---
+++ cinema-salvo.js
@@ -0,0 +1,49 @@
+let catalogo = [
+  {
+    codigo: 1,
+    titulo: 'Titanic',
+    duracao: 2,
+    atores: ['julia', 'gabriel', 'maria'],
+    anoDelancamento: 1996,
+    emCartaz: false
+  },
+  {
+    codigo: 2,
+    titulo: 'Spider Man',
+    duracao: 2,
+    atores: ['mario', 'jose', 'joana'],
+    anoDelancamento: 2010,
+    emCartaz: true
+  }
+]
+
+catalogo.push({
+  codigo: 3,
+  titulo: 'Filme3',
+  duracao: 120,
+  atores: ['Ator1', 'Ato3'],
+  anoDeLancamento: 2025,
+  emCartaz: true
+})
+
+/* console.log(catalogo) */
+
+/* catalogo.push({
+  codigo: 3,
+  titulo: 'Filme3',
+  duracao: 120,
+  atores: ['Ator1', 'Ato3'],
+  anoDeLancamento: 2025,
+  emCartaz: true
+})
+console.log(catalogo) */
+
+/* for (let index = 0; index < catalogo.length; index++) {
+  console.log(catalogo[0]) */
+
+/*for (let index = 0; index < catalogo.length; index++) {
+  const element = catalogo[0]
+  if (element == 'emCartaz') {
+    console.log(!emCartaz)
+  }
+}*/
